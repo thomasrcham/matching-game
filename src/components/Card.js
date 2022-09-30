100465e60f907833bae7b642934cb51cb5dd0307
thomasrcham <guseldorph@gmail.com>
2022-09-30 16:21:55 -0500

    tsaa push it. push it good.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,7 @@
 function Card({ cardSets }) {
   let thing = cardSets[0].frontCard;
   console.log({ thing });
-  return (
-    <img
-      src="./images/pumpkins/pumpkins-card-front.png"
-      alt={cardSets[0].setName}
-    />
-  );
+  return <img src={thing} alt={cardSets[0].setName} />;
 }
 
 export default Card;

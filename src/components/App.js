22596cc78d59c4bfd7d9589853c9afb4c96ba35a
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-30 14:39:40 -0400

    image working

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
+import pumpkinImg1 from '../images/pumpkins/pumpkins-1.png'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={pumpkinImg1} />
         <a
           className="App-link"
           href="https://reactjs.org"

64ab5d3acca119e29decdc78ee0c5b227329d566
Deryn Brown <Deryn.2017@gmail.com>
2022-10-02 15:33:11 -0500

    css cool stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,31 @@
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <h1>
+      <span class="gradient-text">Memories of Autumn</span>
+    </h1>
+  );
+  // return (
+  //   <header>
+  //     <span style={{ color: '#A52A2A' }}>M</span>
+  //     <span style={{ color: '#F4A460' }}>E</span>
+  //     <span style={{ color: '#A0522D' }}>M</span>
+  //     <span style={{ color: '#FF6347' }}>O</span>
+  //     <span style={{ color: '#8B4513' }}>R</span>
+  //     <span style={{ color: '#FFA500' }}>I</span>
+  //     <span style={{ color: '#D2691E' }}>E</span>
+  //     <span style={{ color: '#800000' }}>S</span>
+  //     <span> </span>
+  //     <span style={{ color: '#A52A2A' }}>O</span>
+  //     <span style={{ color: '#F4A460' }}>F</span>
+  //     <span> </span>
+  //     <span style={{ color: '#A0522D' }}>A</span>
+  //     <span style={{ color: '#FF6347' }}>U</span>
+  //     <span style={{ color: '#8B4513' }}>T</span>
+  //     <span style={{ color: '#FFA500' }}>U</span>
+  //     <span style={{ color: '#D2691E' }}>M</span>
+  //     <span style={{ color: '#800000' }}>N</span>
+  //   </header>
+  // )
 }
 
 export default Header;

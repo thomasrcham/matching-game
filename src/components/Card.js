ffae714e609963c5a2d576c26269bdc1bd1ef0eb
thomasrcham <guseldorph@gmail.com>
2022-10-02 16:20:48 -0500

    cards exist, needs flex grid work

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,19 @@ function Card({ card, frontCard, setName }) {
   const [clicked, setClicked] = useState(false);
 
   return clicked ? (
-    <img src={card.image} alt={card.alt} onClick={() => setClicked(false)} />
+    <img
+      src={card.image}
+      alt={card.alt}
+      onClick={() => setClicked(false)}
+      className="card"
+    />
   ) : (
-    <img src={frontCard} alt={setName} onClick={() => setClicked(true)} />
+    <img
+      src={frontCard}
+      alt={setName}
+      onClick={() => setClicked(true)}
+      className="card"
+    />
   );
 }
 
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,17 +1,37 @@
 import Card from "./Card.js";
+import { useEffect, useState } from "react";
+
 function CardContainer({ set, cardsID }) {
-  console.log(set);
-  let displayCards = set
-    ? set.cards.map((card) => (
+  let cardArray = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
+  const [table, setTable] = useState([]);
+
+  let arrayCards = set
+    ? cardArray.map((id) => (
         <Card
-          card={card}
+          card={set.cards[id]}
           setName={set.setName}
           frontCard={set.frontCard}
-          key={card.id}
+          key={set.cards[id]}
         />
       ))
     : null;
-  return <div className="cards">{displayCards}</div>;
+
+  console.log(table);
+
+  let displayCards = set
+    ? set.cards.map((card) => (
+        <td>
+          <Card
+            card={card}
+            setName={set.setName}
+            frontCard={set.frontCard}
+            key={card.id}
+          />
+        </td>
+      ))
+    : null;
+
+  return <div className="displayWrapper">{arrayCards}</div>;
   // return null;
 }
 

569dff63c4e1fdf2432a406cc15864cb30904ac7
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-03 11:55:13 -0400

    refactored to Game.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,9 @@ import { useEffect, useState } from "react";
 import Header from "./Header";
 import Game from "./Game";
 
-function App() {
-  const [cards, setCards] = useState(null);
-  const [cardSetID, setCardSetID] = useState(0);
 
-  useEffect(() => {
-    fetch("http://localhost:3001/cardSets")
-      .then((r) => r.json())
-      .then((d) => setCards(d));
-  }, []);
+
+function App() {
 
   return (
     <div className="App">
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,14 @@ import CardContainer from "./CardContainer";
 import Card from "./Card";
 
 function Game() {
+    const [cards, setCards] = useState(null);
+    const [cardSetID, setCardSetID] = useState(0);
 
+    useEffect(() => {
+        fetch("http://localhost:3001/cardSets")
+            .then((r) => r.json())
+            .then((d) => setCards(d));
+    }, []);
 
     return (<div>
         <div className="sidebar">

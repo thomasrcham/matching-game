7bd078f4df6261624972a048a1f2cc8f6aa4ca3b
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-03 12:07:14 -0400

    begin scoring component

---
+++ src/components/CurrentScore.js
@@ -0,0 +1,5 @@
+function CurrentScore() {
+
+}
+
+export default CurrentScore;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import CardContainer from "./CardContainer";
-import Card from "./Card";
+import CurrentScore from "./CurrentScore";
 
 function Game() {
+    const backend = "http://localhost:3001";
     const [cards, setCards] = useState(null);
     const [cardSetID, setCardSetID] = useState(0);
 
     useEffect(() => {
-        fetch("http://localhost:3001/cardSets")
+        fetch(`${backend}/cardSets`)
             .then((r) => r.json())
             .then((d) => setCards(d));
     }, []);
 
     return (<div>
         <div className="sidebar">
-            <Sidebar />
+            <Sidebar CurrentScore={CurrentScore} />
         </div>
         <div className="mainWindow">
             {cards ? <CardContainer set={cards[cardSetID]} /> : null}

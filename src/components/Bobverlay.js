4116d50605ea5e07c31b30c4d00c4959e891145a
thomasrcham <guseldorph@gmail.com>
2022-10-04 13:45:54 -0500

    timer in overlay testing component

--- src/components/Bobverlay.js
+++ src/components/Bobverlay.js
@@ -3,9 +3,11 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import { Overlay, Classes, Button } from "@blueprintjs/core";
 
 import { NavLink } from "react-router-dom";
+import MyStopwatch from "./Stopwatch";
 
 function Bobverlay() {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <div
@@ -24,6 +26,7 @@ function Bobverlay() {
         >
           Toggle Overlay
         </Button>
+        <MyStopwatch />
 
         <Overlay className={Classes.OVERLAY_SCROLL_CONTAINER} isOpen={isOpen}>
           <div className="overlay">

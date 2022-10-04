d02fe1a5914cf4e380478e33c228d2fbe5a47b59
thomasrcham <guseldorph@gmail.com>
2022-10-03 19:54:20 -0500

    working routing

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,26 +1,37 @@
 import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import CardContainer from "./CardContainer";
-import Card from "./Card";
+import HighScores from "./HighScores";
+import History from "./History";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function Game() {
-    const [cards, setCards] = useState(null);
-    const [cardSetID, setCardSetID] = useState(0);
+  const [cards, setCards] = useState(null);
+  const [cardSetID, setCardSetID] = useState(0);
 
-    useEffect(() => {
-        fetch("http://localhost:3001/cardSets")
-            .then((r) => r.json())
-            .then((d) => setCards(d));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3001/cardSets")
+      .then((r) => r.json())
+      .then((d) => setCards(d));
+  }, []);
 
-    return (<div>
-        <div className="sidebar">
-            <Sidebar />
-        </div>
-        <div className="mainWindow">
-            {cards ? <CardContainer set={cards[cardSetID]} /> : null}
-        </div>
+  return (
+    <div>
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <div className="mainWindow">
+        <Route exact path="/">
+          {cards ? <CardContainer set={cards[cardSetID]} /> : null}
+        </Route>
+        <Route path="/HighScores">
+          <HighScores />
+        </Route>
+        <Route path="/History">
+          <History />
+        </Route>
+      </div>
     </div>
-    );
+  );
 }
 export default Game;

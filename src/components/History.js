fc309390f46a8859dff21768dda932a15cf7bd20
thomasrcham <guseldorph@gmail.com>
2022-10-04 09:34:52 -0500

    adding user history examples to db

--- src/components/History.js
+++ src/components/History.js
@@ -1,5 +1,25 @@
 function History() {
-  return <h1>History</h1>;
+  // let scoresToDisplay = sortedScoresArray.map((entry) => (
+  //   <tr key={entry.score}>
+  //     <td>{entry.user}</td>
+  //     <td>{entry.score}</td>
+  //   </tr>
+  // ));
+
+  // return (
+  //   <div className="dataDisplay">
+  //     <h1>Previous Gamesw</h1>
+  //     <table>
+  //       <thead>
+  //         <tr>
+  //           <th>User</th>
+  //           <th>Score</th>
+  //         </tr>
+  //       </thead>
+  //       {/* <tbody>{scoresToDisplay}</tbody> */}
+  //     </table>
+  //   </div>
+  return null;
 }
 
 export default History;

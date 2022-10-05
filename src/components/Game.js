e0d418a6e8aff044fe18156f80032fb0c6ebde5f
thomasrcham <guseldorph@gmail.com>
2022-10-05 08:30:17 -0500

    post-merge cleanup

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,9 @@
 //libraries
 import { useEffect, useState } from "react";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Bobverlay from "./Bobverlay";
-import { useStopwatch } from "react-timer-hook"
+import { useStopwatch } from "react-timer-hook";
 //Game Components
 import CardContainer from "./CardContainer";
 import CurrentScore from "./CurrentScore";
@@ -11,7 +11,6 @@ import CurrentScore from "./CurrentScore";
 import HighScores from "./HighScores";
 import History from "./History";
 import Sidebar from "./Sidebar";
-
 
 function Game() {
   //variables
@@ -22,42 +21,21 @@ function Game() {
   //timer state
   const [calledTimerValue, setCalledTimerValue] = useState("unset");
 
+  // current gamestate
+  const [flipped, setFlipped] = useState(null);
+  const [matched, setMatched] = useState(null);
+  const [movesCount, setMovesCount] = useState(0);
+  const [newGame, setNewGame] = useState(false);
+  const [shuffledDeck, setShuffledDeck] = useState(null);
 
-    // current gamestate
-    const [flipped, setFlipped] = useState(null);
-    const [matched, setMatched] = useState(null);
-    const [movesCount, setMovesCount] = useState(0);
-    const [newGame, setNewGame] = useState(false);
-    const [shuffledDeck, setShuffledDeck] = useState(null);
+  //scoring
+  const [highScores, setHighScores] = useState(null);
+  const [userHistory, setUserHistory] = useState(null);
 
-    //scoring
-    const [highScores, setHighScores] = useState(null);
-    const [userHistory, setUserHistory] = useState(null);
-        
-    //overlay state
-    const [isOpen, setIsOpen] = useState(false);
+  //overlay state
+  const [isOpen, setIsOpen] = useState(false);
 
-    const { seconds, minutes, isRunning, start, pause, reset } = useStopwatch({
-        autoStart: false,
-    });
-
-    // side effects
-    useEffect(() => {
-        fetch(`${backend}/highScores`)
-            .then((r) => r.json())
-            .then((d) => setHighScores(d));
-    }, []);
-
-  //select and load decks
-  const [decks, setDecks] = useState(null);
-  const [deckId, setDeckId] = useState(0);
-
-  // timer hook
-  const { seconds, minutes, start, pause, reset } = useStopwatch({
-    autoStart: false,
-  });
-
-
+  // side effects
   useEffect(() => {
     fetch(`${backend}/highScores`)
       .then((r) => r.json())
@@ -70,36 +48,52 @@ function Game() {
       .then((d) => setDecks(d));
   }, []);
 
+  useEffect(() => {
+    fetch(`${backend}/userHistory`)
+      .then((r) => r.json())
+      .then((d) => setUserHistory(d));
+  }, []);
 
-    //game logic
+  //select and load decks
+  const [decks, setDecks] = useState(null);
+  const [deckId, setDeckId] = useState(0);
 
-    function handleTimerToScore() {
-        let totalTime = minutes * 60 + seconds
-        console.log(totalTime)
+  // timer hook
+  const { seconds, minutes, start, pause, reset } = useStopwatch({
+    autoStart: false,
+  });
+
+  function handleTimerValueSet() {
+    let totalTime = minutes * 60 + seconds;
+    setCalledTimerValue(totalTime);
+  }
+
+  //GAME LOGIC
+
+  //shuffle the cards before handing to component because React hates conditionally calling hooks (and you can't shuffle until the deck is available)
+
+  function shuffleDeck(deckToShuffle) {
+    let shuffledCards = [...deckToShuffle, ...deckToShuffle]; //duplicates the deck
+    // https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledCards[i], shuffledCards[j]] = [
+        shuffledCards[j],
+        shuffledCards[i],
+      ];
     }
+    return shuffledCards;
+  }
 
-    //shuffle the cards before handing to component because React hates conditionally calling hooks (and you can't shuffle until the deck is available)
-
-    function shuffleDeck(deckToShuffle) {
-        let shuffledCards = [...deckToShuffle, ...deckToShuffle]; //duplicates the deck
-        // https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
-        for (let i = shuffledCards.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
-        }
-        return shuffledCards;
+  useEffect(() => {
+    if (decks !== null && deckId !== null) {
+      let singleDeck = decks[deckId];
+      setShuffledDeck([...shuffleDeck(singleDeck.cards)]);
+      console.log({ shuffledDeck });
     }
+  }, [newGame]);
 
-
-    useEffect(() => {
-        if (decks !== null && deckId !== null) {
-            let singleDeck = decks[deckId];
-            setShuffledDeck([...shuffleDeck(singleDeck.cards)]);
-            console.log({ shuffledDeck })
-        }
-    }, [newGame])
-
-    /*
+  /*
     flipping
         adds one to number of moves
         adds cards to an array.
@@ -110,45 +104,46 @@ function Game() {
         if unequal
           remove from array */
 
-    function handleFlip(cardClickEvent) {
-        console.log({ cardClickEvent })
-        setMovesCount(movesCount + 1);
-        console.log({ movesCount });
-        return null;
-    }
+  function handleFlip(cardClickEvent) {
+    console.log({ cardClickEvent });
+    setMovesCount(movesCount + 1);
+    console.log({ movesCount });
+    return null;
+  }
 
-    return (
-        <div>
-            <div className="sidebar">
-                <Sidebar
-                    CurrentScore={CurrentScore}
-                    minutes={minutes}
-                    newGame={newGame}
-                    setNewGame={setNewGame}
-                    seconds={seconds}
-                />
-            </div >
-            <div className="mainWindow">
-                <Route exact path="/">
-                    {decks ? (
-                        <CardContainer
-                            decks={decks}
-                            deckId={deckId}
-                            flipped={flipped}
-                            handleFlip={handleFlip}
-                            setFlipped={setFlipped}
-                            matched={matched}
-                            setMatched={setMatched}
-                            newGame={newGame}
-                            shuffledDeck={shuffledDeck}
-                        />
-                    ) : null}
-                </Route>
-                <Route path="/HighScores">
-                    {highScores ? <HighScores highScoresArray={highScores} /> : null}
-                </Route>
-                <Route path="/History">
-                    {userHistory ? <History userHistory={userHistory} /> : null}
+  return (
+    <div>
+      <div className="sidebar">
+        <Sidebar
+          CurrentScore={CurrentScore}
+          minutes={minutes}
+          newGame={newGame}
+          setNewGame={setNewGame}
+          seconds={seconds}
+        />
+      </div>
+      <div className="mainWindow">
+        <Route exact path="/">
+          {decks ? (
+            <CardContainer
+              decks={decks}
+              deckId={deckId}
+              flipped={flipped}
+              handleFlip={handleFlip}
+              setFlipped={setFlipped}
+              matched={matched}
+              setMatched={setMatched}
+              newGame={newGame}
+              shuffledDeck={shuffledDeck}
+            />
+          ) : null}
+        </Route>
+        <Route path="/HighScores">
+          {highScores ? <HighScores highScoresArray={highScores} /> : null}
+        </Route>
+        <Route path="/History">
+          {userHistory ? <History userHistory={userHistory} /> : null}
+        </Route>
         <Route path="/Bobverlay">
           <Bobverlay
             handleTimerValueSet={handleTimerValueSet}
@@ -160,20 +155,9 @@ function Game() {
             calledTimerValue={calledTimerValue}
           />
         </Route>
-            </div>
-        </div >
-    );
-    
-    useEffect(() => {
-    fetch(`${backend}/userHistory`)
-      .then((r) => r.json())
-      .then((d) => setUserHistory(d));
-  }, []);
-
-  function handleTimerValueSet() {
-    let totalTime = minutes * 60 + seconds;
-    setCalledTimerValue(totalTime);
-  }
-
+      </div>
+    </div>
+  );
 }
+
 export default Game;

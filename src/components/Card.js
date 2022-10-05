d5b3422b3850da62a4c4d131d79239f6326b4f90
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-05 12:20:46 -0400

    breaking out clicked vs amIFlipped

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,34 @@ import { useState, useEffect } from "react";
 
 function Card({ card, cardBack, flipped, handleFlip, setName, matched }) {
   const [amIFlipped, setAmIFlipped] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     console.log("a card was clicked")
-  }, [amIFlipped]);
+    if (flipped !== []) {
+      // if (flipped.filter(card.flippedid) ) {
+      //   console.log(`found card id ${card.flippedid}in flipped array`)
+      // }
+      let checkIfFlipped = flipped.find((flippedElement) => {
+        if (flippedElement === card.flippedid) {
+          setAmIFlipped(true);
+        }
+      });
+      console.log({ checkIfFlipped });
+    }
+  }, [clicked]);
+
+
 
   return <img
     className="card"
     cardid={card.id} //adds card value to event for handleflip
     flippedid={card.flippedid} //adds card id to event for handleflip
-    src={amIFlipped ? card.image : cardBack}
+    src={clicked ? card.image : cardBack}
     alt={card.alt}
     onClick={(event) => {
       handleFlip(event);
-      setAmIFlipped(!amIFlipped);
+      setClicked(!clicked);
     }}
   />
 
